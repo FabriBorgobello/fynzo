78fcd4573a25066e30dc69912325dbf6fe4c6cc8
Fabricio Borgobello <fborgobello@gmail.com>
2025-05-27 00:26:46 +0200

    ESLint, prettier, husky and lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,10 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import eslintConfigPrettier from "eslint-config-prettier";
+import drizzle from "eslint-plugin-drizzle";
+import prettierPlugin from "eslint-plugin-prettier/recommended";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +15,39 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslintConfigPrettier,
+  prettierPlugin, // Prettier should be last
+  {
+    plugins: { drizzle, "simple-import-sort": simpleImportSort },
+    rules: {
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react$", "^react-dom$"], // React and ReactDOM
+            ["^@?\\w"], // External packages
+            ["^@/"], // Aliased imports
+            ["^\\."], // Relative imports
+            ["^.+\\.?(css|scss|sass|less)$"], // Style imports
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "error",
+      ...drizzle.configs.recommended.rules,
+      "drizzle/enforce-delete-with-where": [
+        "error",
+        { drizzleObjectName: ["db"] },
+      ],
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
