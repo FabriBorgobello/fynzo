67692e7e6cbcb5ace46785080fbb3fe8cae99c88
Fabricio Borgobello <fborgobello@gmail.com>
2025-05-27 00:21:46 +0200

    Database URL

--- src/env/server.ts
+++ src/env/server.ts
@@ -2,7 +2,9 @@ import { z } from "zod/v4";
 
 const envSchema = z.object({
   ENVIRONMENT: z.enum(["development", "staging", "production"]),
+  DATABASE_URL: z.url(),
 });
 export const env = envSchema.parse({
   ENVIRONMENT: process.env.ENVIRONMENT,
+  DATABASE_URL: process.env.DATABASE_URL,
 });
