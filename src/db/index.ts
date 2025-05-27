1c25057f9485db600f6b88516df707d6370863ca
Fabricio Borgobello <fborgobello@gmail.com>
2025-05-27 23:51:15 +0200

    Logger off

--- src/db/index.ts
+++ src/db/index.ts
@@ -7,9 +7,6 @@ import { files } from "./schema/files";
 
 export const db = drizzle(env.DATABASE_URL, {
   casing: "snake_case", // https://orm.drizzle.team/docs/sql-schema-declaration#camel-and-snake-casing
-  logger: true,
-  schema: {
-    files,
-    chunks,
-  },
+  // logger: true,
+  schema: { files, chunks },
 });
