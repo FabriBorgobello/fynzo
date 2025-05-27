efeae3cc3d77cf03fa1f6c0ccfaaf788ba1e85a1
Fabricio Borgobello <fborgobello@gmail.com>
2025-05-27 22:31:12 +0200

    Update table

---
+++ drizzle/0002_zippy_venus.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "files" ALTER COLUMN "processed_at" SET DATA TYPE timestamp;
--- src/db/schema/files.ts
+++ src/db/schema/files.ts
@@ -6,6 +6,6 @@ export const files = pgTable("files", {
   id: uuid("id").primaryKey().defaultRandom(),
   name: text("name").notNull(), // filename
   etag: text("etag").notNull(), // hash or identifier from MinIO
-  processedAt: timestamp("processed_at", { withTimezone: true }),
+  processedAt: timestamp({ mode: "string" }),
   ...timestamps,
 });
